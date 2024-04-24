fb9034776da7c0cf8c9ac7f37e145c34bcfa92d5
Mordween <al.ridolfi@laposte.net>
2024-04-24 16:11:38 +0200

    Z axis bug fixes

--- src/robots.ts
+++ src/robots.ts
@@ -3,6 +3,7 @@ import { ExtendedObject3D, Scene3D} from 'enable3d';
 
 // Define the structure for each robot part
 export interface RobotPart {
+  joint: string;     // name of the joint
   mesh: string;      // Path to the .glb file
   t: number[];       // Translation vector [x, y, z]
   q: number[];       // Quaternion [q0, q1, q2, q3]
@@ -13,22 +14,34 @@ export interface RobotDictionary {
   [key: string]: RobotPart;
 }
 
-// Function to load a robot into a scene
-export async function loadRobot(robotData: RobotDictionary, scene: Scene3D) {
-  
+// // Function to load a robot into a scene
+export async function loadRobot(robotData: RobotDictionary, robotLinks: Object, scene: Scene3D) {
 
   for (const key in robotData) {
-    const part = robotData[key];
-    await scene.load.gltf(part.mesh).then(gltf => {
+    const dicoPart = robotData[key];
+    const partLink = (robotLinks as any)['links'][key];
+    const partJoint = (robotLinks as any)['joints'][dicoPart.joint]
+    // console.log(partJoint)
+    //console.log(part)
+    await scene.load.gltf(dicoPart.mesh).then(gltf => {
       let object = new ExtendedObject3D()
       const mesh = gltf.scene.children[0]
-      
-      object.position.set(part.t[0], part.t[1], part.t[2])
-      object.quaternion.set(part.q[0], part.q[1], part.q[2], part.q[3])
+    
+      if(Number.isNaN(dicoPart.joint) || dicoPart.joint === 'NaN')
+      {
+        object.position.set(0,0,0)
+      }
+      else
+      {
+        object.position.set(partJoint.position['x'], partJoint.position['y'], partJoint.position['z'])
+        // console.log(partJoint.position)
+      }   
+      object.quaternion.set(partLink.quaternion['_x'], partLink.quaternion['_y'], partLink.quaternion['_z'], partLink.quaternion['_w'])
       object.add(mesh)
 
+      console.log(object)
       scene.add.existing(object)
-      //scene.physics.add.existing(object, { shape: 'mesh' })
+      // scene.physics.add.existing(object, { shape: 'mesh' })
     });
 }
 }
--- src/scene.ts
+++ src/scene.ts
@@ -1,6 +1,82 @@
 import { Project, Scene3D, PhysicsLoader, THREE, /*ExtendedObject3D */} from "enable3d";
 import { loadRobot, RobotDictionary } from "./robots.ts";
 
+import URDFLoader from 'urdf-loader';
+import { LoadingManager } from 'three';
+
+const robotManager = new LoadingManager();
+const robotLoader = new URDFLoader( robotManager );
+
+
+// Define the robot data
+const robotData: RobotDictionary = {
+  "base": {
+    "joint" : NaN.toString(),
+    "mesh": "src/assets/base.glb",
+    "t": [0, 0, 0],
+    "q": [0, 0, 0, 1]
+  },
+  "link1": {
+    "joint" : "joint1",
+    "mesh": "src/assets/link1.glb",
+    "t": [0.0, 0.0, 0.2435],
+    "q": [0.0, 0.0, 0.0, 1.0]
+  },
+  "link2": {
+    "joint" : "joint2",
+    "mesh": "src/assets/link2.glb",
+    "t": [0.0, 0.0, 0.2435],
+    "q": [-0.4999981410979567, -0.5000018143028963, -0.4999963044921138, 0.5000036954879761]
+  },
+  "link3": {
+    "joint" : "joint3",
+    "mesh": "src/assets/link3.glb",
+    "t": [7.353756616701401e-07, 5.402371266756581e-12, 0.4436999999986494],
+    "q": [0.7071041706433439, 6.493365701876792e-06, 1.2986979459196817e-06, 0.7071093916893005]
+  },
+  "link4": {
+    "joint" : "joint4",
+    "mesh": "src/assets/link4.glb",
+    "t": [0.08699906327003075, -7.134198477910336e-07, 0.21608936087324546],
+    "q": [1.0, 5.5098144010580686e-06, -3.673194983895874e-06, 1.8366160440284816e-06]
+  },
+  "link5": {
+    "joint" : "joint5",
+    "mesh": "src/assets/link5.glb",
+    "t": [0.08699906327003075, -7.134198477910336e-07, 0.21608936087324546],
+    "q": [-0.7071067932571579, -1.2986741408059922e-06, 6.493370704012438e-06, 0.7071067690849304]
+  },
+  "link6": {
+    "joint" : "joint6",
+    "mesh": "src/assets/link6.glb",
+    "t": [0.08699860411939285, -7.134240641771995e-07, 0.15358936087493202],
+    "q": [1.0, 5.509814401058069e-06, -3.6731949838958737e-06, 1.8366160440284816e-06]
+  },
+};
+
+function loadURDF(scene: Scene3D)
+{
+    robotLoader.packages = {
+        packageName : './package/dir/'              // The equivalent of a (list of) ROS package(s):// directory
+    };
+    robotLoader.load(
+      'ufactory_description/lite6/urdf/lite6.urdf', // The path to the URDF within the package OR absolute                       
+      robot => {
+
+        console.log(robot);
+
+        //loadRobot(robotData, robot, scene);
+    
+        // The robot is loaded!
+        
+        scene.add.existing(robot)
+
+        // scene.physics.add.existing(robot, { shape: 'mesh' })
+    
+      }
+    );
+}
+
 class MainScene extends Scene3D {
   box: any;
   constructor() {
@@ -45,8 +121,6 @@ class MainScene extends Scene3D {
     // set up scene (light, ground, grid, sky, orbitControls)
     this.warpSpeed("light", "camera", "lookAtCenter", "grid", "ground",  "orbitControls", "fog", "sky");  
 
-    this.camera.up.set(0,0,1)         // Z up
-
     // enable physics debug
     this.physics.debug?.enable();
 
@@ -69,47 +143,8 @@ class MainScene extends Scene3D {
     //@ts-ignore
     this.physics.add.existing(cube);
 
-    // Define the robot data
-    const robotData: RobotDictionary = {
-      "base": {
-        "mesh": "src/assets/base.glb",
-        "t": [0, 0, 0],
-        "q": [0, 0, 0, 1]
-      },
-      "link1": {
-        "mesh": "src/assets/link1.glb",
-        "t": [0.0, 0.0, 0.2435],
-        "q": [0.0, 0.0, 0.39512347562412636, 0.9186280369758606]
-      },
-      "link2": {
-        "mesh": "src/assets/link2.glb",
-        "t": [0.0, 0.0, 0.2435],
-        "q": [-0.6663108970388383, -0.23670611523012658, 0.3209148965605773, 0.6300901770591736]
-      },
-      "link3": {
-        "mesh": "src/assets/link3.glb",
-        "t": [0.0, 0.0, 0.2435],
-        "q": [-0.6663108970388383, -0.23670611523012658, 0.3209148965605773, 0.6300901770591736]
-      },
-      "link4": {
-        "mesh": "src/assets/link4.glb",
-        "t": [0.0, 0.0, 0.2435],
-        "q": [-0.6663108970388383, -0.23670611523012658, 0.3209148965605773, 0.6300901770591736]
-      },
-      "link5": {
-        "mesh": "src/assets/link5.glb",
-        "t": [0.0, 0.0, 0.2435],
-        "q": [-0.6663108970388383, -0.23670611523012658, 0.3209148965605773, 0.6300901770591736]
-      },
-      "link6": {
-        "mesh": "src/assets/link6.glb",
-        "t": [0.0, 0.0, 0.2435],
-        "q": [-0.6663108970388383, -0.23670611523012658, 0.3209148965605773, 0.6300901770591736]
-      },
-    };
-
     // Load the robot
-    loadRobot(robotData, this);
+    loadURDF(this);
     
   }
   
