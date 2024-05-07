858056671b3f9880769621231ff850ceec424886
Mordween <al.ridolfi@laposte.net>
2024-05-07 10:54:52 +0200

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/enable3d-ammojs-vite-example/",
+  server: 
+  {
+    port : 3000,
+    open: 'app',
+  },
 });
